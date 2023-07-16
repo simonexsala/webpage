20874072ce55669b7c1deee4848bcd6428a64991
Simone Sala <simonexsala@gmail.com>
2023-07-16 23:10:17 +0200

    added content

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,27 +3,16 @@
     <div class="container flex-1 max-w-3xl mx-auto xl:max-w-5xl">
       <NuxtImg src="tiger.jpg" class="rounded-3xl object-cover" />
       <div class="mt-4">
-        <h1 class="text-3xl font-bold sm:text-4xl lg:text-5xl">
-          Come rendere l'uomo forte e potente?
+        <h1 class="text-2xl min-[381px]:text-3xl font-bold sm:text-4xl lg:text-5xl">
+          Istinto, energia, natura
         </h1>
 
         <div class="flex leading-relaxed text-gray-700 mt-2">
           <article>
             <p>
-              Come quando, in un luogo solitario dove l'acqua scorre 
-              abbondante, un uomo cresce una pianta d'olivo fiorente, 
-              bella, vigorosa, coperta di fiori bianchi, 
-              che si muove al soffio dei venti; 
-              ma all'improvviso una tempesta la sradica da terra e 
-              l'abbatte al suolo...
-              <br />
-              Come quando un leone dei monti, superbo della sua forza, 
-              da una mandria che pascola rapisce la mucca più bella; 
-              e prima le spezza il collo afferrandola con i denti 
-              robusti, poi la sbrana e ne divora le viscere 
-              insanguinate; intorno a lui abbaiano i cani, 
-              gridano i pastori da lontano e non osano farsi 
-              avanti, il livido terrore li coglie... 
+              Volontà totale di liberazione dai detriti tossici di false religioni e filosofie,
+              un salire in alto, in alto, verso l'alta, libera, magari terribile natura,
+              la natura che gioca e può giocare coi grandi compiti...
             </p>
           </article>
         </div>
